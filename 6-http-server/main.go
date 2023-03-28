9cc5158009ebbb3324a9f6515da56a3485f819cd
Luiz Henrique <luizhjramos@outlook.com>
2023-03-27 21:07:23 -0300

    feat: headers manipulation studies

--- 6-http-server/main.go
+++ 6-http-server/main.go
@@ -3,10 +3,23 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", SearchZipCode)
+	http.HandleFunc("/", SearchZipCodeHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func SearchZipCode(response http.ResponseWriter, request *http.Request) {
+func SearchZipCodeHandler(response http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		response.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cepParam := request.URL.Query().Get("cep")
+	if cepParam == "" {
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
 	response.Write([]byte("Hello world!"))
 }
